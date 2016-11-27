2260e221edf51322ca9c43c58ad2e6ecc3a85fbc
Karol Stępień <karol.stepien@me.com>
2016-11-27 16:44:28 +0100

    project init

---
+++ Regease/Regease/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Regease
+//
+//  Created by Karol Stępień on 27.11.2016.
+//  Copyright © 2016 Regease. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
